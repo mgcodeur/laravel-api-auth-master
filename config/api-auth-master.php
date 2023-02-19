9c4f9b73a1e1ad0595cedabca2c6dcc48b63b0c7
mgcodeur <mgcodeur@gmail.com>
2023-02-19 09:33:46 +0300

    edit config

--- config/api-auth-master.php
+++ config/api-auth-master.php
@@ -1,14 +1,12 @@
 <?php
 
-use App\Models\User;
-
 return [
     'use' => [
         'jobs' => true, // don't forget to run php artisan queue:table and php artisan migrate and php artisan queue:work if you want to use jobs
     ],
 
     'auth' => [
-        'model' => User::class, // model used for authentication, you can replace it
+        'model' => 'App\Models\User', // model used for authentication, you can replace it
         'table' => 'users',
     ],
 ];
