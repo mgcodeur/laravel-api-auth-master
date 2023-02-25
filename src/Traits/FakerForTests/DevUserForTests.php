c5d2e1d92c8f32e07761d10b3f00f7c65b0d91f6
mgcodeur <mgcodeur@gmail.com>
2023-02-25 13:45:07 +0300

    refactor tests, create DevUserFactory

---
+++ src/Traits/FakerForTests/DevUserForTests.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mgcodeur\LaravelApiAuthMaster\Traits\FakerForTests;
+
+class DevUserForTests
+{
+    public static function generateFakeUser()
+    {
+        $faker = \Faker\Factory::create('Fr_fr');
+
+        return [
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'email' => $faker->unique()->safeEmail,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+    }
+}
